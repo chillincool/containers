25233e61825db84ada344d5ff323630298c9419a
chillincool <chris@chillincool.net>
2025-11-23 15:53:24 -0600

    yay

--- apps/radarr/entrypoint.sh
+++ apps/radarr/entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 # Expand environment variables in config.xml.tmpl
 if [[ ! -f /config/config.xml ]]; then
-    echo "Generating config.xml from template..."
-    gomplate -f /config.xml.tmpl -o /config/config.xml --datasource env
+	echo "Generating config.xml from template..."
+	gomplate -f /config.xml.tmpl -o /config/config.xml --datasource env
 fi
 
 # Start Radarr
